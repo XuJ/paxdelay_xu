eb4369b2fb370792af1bbedfbd66b1da7fd40cdc
tansaris <tan4778@gmail.com>
2016-10-11 21:56:39 -0400

    Update create DB1B

--- sql/mysql/foundation/m_CreateDB1BCouponsTable.sql
+++ sql/mysql/foundation/m_CreateDB1BCouponsTable.sql
@@ -17,37 +17,13 @@ create table db1b_coupons
 	year	numeric(4) not null,
 	quarter	int not null,
 	origin	char(3) not null,
-	number_origin_airports	numeric(2, 0) not null,
-	
-		origin_airport_seqid int,
-	origin_city_code	numeric(6, 0) not null,
-	origin_country_code	varchar(3) not null,
-	origin_state_fips	numeric(2, 0) not null,
-	origin_state	char(2) not null,
-	origin_state_name	varchar(50) not null,
-	origin_wac	numeric(4, 0) not null,
 	destination	char(3) not null,
 	number_destination_airports	numeric(2, 0) not null,
-	
-		dest_airport_seqid int,
-	destination_city_code	numeric(6, 0) not null,
-	destination_country_code	varchar(3) not null,
-	destination_state_fips	numeric(2, 0) not null,
-	destination_state	char(2) not null,
-	destination_state_name	varchar(50) not null,
-	destination_wac	numeric(4, 0) not null,
-	break_code	char(1),
-	coupon_type	char(1) not null,
 	ticketing_carrier	varchar(3) not null,
 	operating_carrier	varchar(3) not null,
 	reporting_carrier	varchar(3) not null,
 	passengers	numeric(4, 0) not null,
 	fare_class	char(1),
-	distance	numeric(4, 0) not null,
-	distance_group	numeric(2, 0) not null,
-	gateway_flag	numeric(1, 0) not null,
-	itinerary_geography_type	numeric(1, 0) not null,
-	coupon_geography_type	numeric(1, 0) not null
 )
 ENGINE = MyISAM
 partition by list (quarter)
